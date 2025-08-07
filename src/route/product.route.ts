6f1369472cf20594b1e5a35b96b74ea9839d5bf0
nusratalam123 <nusratalam975@gmail.com>
2025-08-07 13:25:02 +0600

    update the product route use express async handler

--- src/route/product.route.ts
+++ src/route/product.route.ts
@@ -3,11 +3,16 @@ import express from 'express';
 import { createProduct, getProducts } from '../controller/product.controller';
 import validate from '../middleware/validate';
 import { createProductSchema } from '../validator/product.validator';
+import expressAsyncHandler from 'express-async-handler';
 
 const router = express.Router();
 
-router.post('/create', validate(createProductSchema), createProduct);
-router.get('/all', getProducts);
+router.post(
+    '/create',
+    validate(createProductSchema),
+    expressAsyncHandler(createProduct)
+  );
+  router.get('/all', getProducts);
 
 export default router;
 
